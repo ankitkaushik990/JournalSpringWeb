9442a31e3bbf8815c2186c33c6a15da7d556d0ad
Ankit <2002ankit2002@gmail.com>
2025-10-12 14:22:29 +0530

    add transactions

--- src/main/java/com/edigest/journalApp/service/JournalEntryService.java
+++ src/main/java/com/edigest/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import com.edigest.journalApp.repository.JournalEtryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,12 +22,18 @@ public class JournalEntryService {
     @Autowired
     private  UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEtryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try{
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEtryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
     }
 
 
